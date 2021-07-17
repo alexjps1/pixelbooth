eb1b4edc5612f2812e57d5ad58c73c90b41f793c
Alex JPS <git@alexjps.com>
2021-07-17 11:37:16 -0700

    Add RNG codes and empty config.txt

--- main.sh
+++ main.sh
@@ -1,19 +1,35 @@
+# TODO: detect config.txt file with line 1 $SERVER, line 2 $DIRECTORY
+
 while :
 do
     if [[ $(ls pics) ]]
-    # Check whether there is a file in the pics directory
+    # Check for file in pics dir
     then
         echo "File detected in pics directory"
 
-        # Need to adjust numbers for optimal results
-        # Arguments: input, output, square size, scale, threshold
+        # TODO: fix square size, scale, threshold
         python3 pixelbooth.py pics/* pics/output.png 1 1 1
 
         while :
             if [[ $(ls pics | grep "output.png")]]
-            # Check if output has been generated
+            # Check for generated output
             then
-                # Put rsync & HTML generation commands here
+                echo "Output image detected"
 
+                # Gen random code for file
+                while : 
+                    CODE=$(shuf -n 1 -i 1000-9999)
+
+                    # Regen if pic with code already existing
+                    wget "${SERVER}/${DIRECTORY}/${CODE}.png" && continue
+
+                    # Put rsync & HTML generation commands here
+                    # TODO: input root password to server
+                    rsync -avzP ./pics/output.png root@${SERVER}:${DIRECTORY}/${CODE}.png
+
+                    sleep 1
+                done
+            sleep 1
         done
+    sleep 1
 done
